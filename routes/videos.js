ee6d3f447b8a575b8ee30168f5dc1f543a02ebe6
Azzuwan Aziz <azzuwan@gmail.com>
2016-10-16 15:36:54 +0800

    Dynamically set IP from Raspberry pi to the video page

--- routes/videos.js
+++ routes/videos.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-	res.render('video.html', {});  
+router.get('/:ip', function(req, res, next) {
+	res.render('video.html', {ip: req.params.ip});  
 });
 
 module.exports = router;
